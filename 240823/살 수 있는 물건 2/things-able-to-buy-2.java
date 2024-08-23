4b2f037f00c7358467191335b4117901578917e0
more0st <118714351+more0st@users.noreply.github.com>
2024-08-23 16:06:35 +0900

    Title: 살 수 있는 물건 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240823/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str;
+        if (n >= 3000) {
+            str = "book";
+        }else if (n >= 1000) {
+            str = "mask";
+        }else if (n >= 500) {
+            str = "pen";
+        } else {
+            str = "no";
+        }
+
+        System.out.print(str);
+    }
+}
