e3bbfad2c0944975b6d5c3bd2614fd3980477628
more0st <118714351+more0st@users.noreply.github.com>
2024-08-23 17:40:26 +0900

    Title: 두 사람, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240823/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a_age = sc.nextInt();
+        String a_sex = sc.next();
+        int b_age = sc.nextInt();
+        String b_sex = sc.next();
+
+        if ((a_age >= 19 && a_sex.equals("M")) || (b_age >= 19 && b_sex.equals("M")) ){
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
