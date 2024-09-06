2e8e3805e3ad340d81d9d3143a09f979f7981faa
Jay <118714351+devJay123@users.noreply.github.com>
2024-09-06 11:26:58 +0900

    Title: 배수의 개수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240906/배수의 개수/number-of-multipliers.py
+++ 240906/배수의 개수/number-of-multipliers.py
@@ -5,9 +5,9 @@ for _ in range(10):
     a = int(input())
     if a % 3 == 0 and a % 5 == 0:
         cnt3 += 1
-        cnt 5 += 1
+        cnt5 += 1
     elif a % 3 == 0:
-        cnt 3 += 1
+        cnt3 += 1
     elif a % 5 == 0:
         cnt5 += 1
 
